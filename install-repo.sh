cd69dc5445d9943cab202482109a22bcaf2e81a4
TENET <40796508+luisadha@users.noreply.github.com>
2026-06-14 22:39:42 +0800

    Update repository source and author information

--- install-repo.sh
+++ install-repo.sh
@@ -1,19 +1,16 @@
 #!/data/data/com.termux/files/usr/bin/bash
 # File       : install-repo.sh
-# Author     : rendiix <vanzdobz@gmail.com>
-# Create date:  5-Jul-2019 16:11
+# Author     : luisadha
+# Create date:  15-Jun-2026 
 # Get some needed tools. coreutils for mkdir command, gnugp for the signing key, and apt-transport-https to actually connect to the repo
-pkg i gnupg wget -y
+pkg i wget -y
+#gnupg
 [ ! -d $PREFIX/etc/apt/sources.list.d ] && mkdir $PREFIX/etc/apt/sources.list.d
 # Write the needed source file
 if [ ! -f "$PREFIX/etc/apt/sources.list.d/rendiix.list" ]; then
-	echo -e "deb https://rendiix.github.io android-tools termux" > $PREFIX/etc/apt/sources.list.d/rendiix.list
-	wget -qP $PREFIX/etc/apt/trusted.gpg.d https://rendiix.github.io/rendiix.gpg
+	echo "deb [trusted=yes] https://repo.luisadha.dev/ stable main" > $PREFIX/etc/apt/sources.list.d/luisadha.list
+   # wget -qP $PREFIX/etc/apt/trusted.gpg.d https://repo.luisadha.dev/luisadha.gpg
 	apt update
 else 
 	echo "repo already installed"
 fi 
-
-
-
-
