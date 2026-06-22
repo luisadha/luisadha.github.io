5d95eba5ea633f59085c38a5e8f91cec557361b7
luisadha <adharudin14@gmail.com>
2026-06-22 22:18:56 +0800

    Solve signing error

--- install-repo.sh
+++ install-repo.sh
@@ -1,25 +1,14 @@
-#!/data/data/com.termux/files/usr/bin/bash
-# File       : install-repo.sh
-# Author     : luisadha
+Author     : luisadha
 # Create date:  15-Jun-2026 
 # Get some needed tools. coreutils for mkdir command, gnugp for the signing key, and apt-transport-https to actually connect to the repo
-set -e
+pkg i wget -y gnupg
+[ ! -d $PREFIX/etc/apt/sources.list.d ] && mkdir $PREFIX/etc/apt/sources.list.d
+# Write the needed source file
+if [ ! -f "$PREFIX/etc/apt/sources.list.d/luisadha.list" ]; then
+	echo "deb https://repo.luisadha.dev/ stable main" > $PREFIX/etc/apt/sources.list.d/luisadha.list
+    wget -qP $PREFIX/etc/apt/trusted.gpg.d https://repo.luisadha.dev/luisadha-dev.gpg
+	apt update
+else 
+	echo "repo already installed"
+fi 
 
-pkg install -y wget gnupg
-
-mkdir -p $PREFIX/etc/apt/sources.list.d
-mkdir -p $PREFIX/etc/apt/trusted.gpg.d
-
-REPO_FILE="$PREFIX/etc/apt/sources.list.d/luisadha.list"
-KEY_FILE="$PREFIX/etc/apt/trusted.gpg.d/luisadha-dev.gpg"
-
-if [ ! -f "$REPO_FILE" ]; then
-    echo "deb https://repo.luisadha.dev/ stable main" > "$REPO_FILE"
-
-    wget -qO- https://repo.luisadha.dev/luisadha-dev.gpg \
-        | gpg --dearmor > "$KEY_FILE"
-
-    apt update
-else
-    echo "repo already installed"
-fi
