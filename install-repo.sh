f9028454c1f13ec6f9120cfd7d04b9f13fc45ae8
TENET <40796508+luisadha@users.noreply.github.com>
2026-06-22 21:12:23 +0800

    Fix package installation and update repo source
    Updated package installation command to include gnupg and modified the repository source file to remove 'trusted=yes'.

--- install-repo.sh
+++ install-repo.sh
@@ -3,13 +3,12 @@
 # Author     : luisadha
 # Create date:  15-Jun-2026 
 # Get some needed tools. coreutils for mkdir command, gnugp for the signing key, and apt-transport-https to actually connect to the repo
-pkg i wget -y
-#gnupg
+pkg i wget -y gnupg
 [ ! -d $PREFIX/etc/apt/sources.list.d ] && mkdir $PREFIX/etc/apt/sources.list.d
 # Write the needed source file
 if [ ! -f "$PREFIX/etc/apt/sources.list.d/rendiix.list" ]; then
-	echo "deb [trusted=yes] https://repo.luisadha.dev/ stable main" > $PREFIX/etc/apt/sources.list.d/luisadha.list
-   # wget -qP $PREFIX/etc/apt/trusted.gpg.d https://repo.luisadha.dev/luisadha.gpg
+	echo "deb https://repo.luisadha.dev/ stable main" > $PREFIX/etc/apt/sources.list.d/luisadha.list
+    wget -qP $PREFIX/etc/apt/trusted.gpg.d https://repo.luisadha.dev/luisadha-dev.gpg
 	apt update
 else 
 	echo "repo already installed"
